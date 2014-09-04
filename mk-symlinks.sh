635fe7b6479a0ef5a0f642135412b0c141d4608b
Chris Merck <cmerck@osiris>
2014-09-04 10:38:57 -0700

    Adding global .gitconfig

--- mk-symlinks.sh
+++ mk-symlinks.sh
@@ -1,3 +1,7 @@
+# Creates symlinks to dotfiles here.
+# If a target file alread exists it will
+#  NOT be overwritten, but will need to be
+#  removed manually.
 echo "Creating links to dotfiles..."
 while read p; do
   ln -s -T $(pwd)/$p ~/$p
